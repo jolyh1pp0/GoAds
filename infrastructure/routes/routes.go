2b72742bd73a918a94b3082e4fcf90f0c1519257
jolyh1pp0 <54851461+jolyh1pp0@users.noreply.github.com>
2022-06-19 22:27:13 +0300

    New routers

--- infrastructure/routes/routes.go
+++ infrastructure/routes/routes.go
@@ -10,8 +10,10 @@ func NewRouter(e *echo.Echo, c controller.AppController) *echo.Echo {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/users", func(context echo.Context) error { return c.Advertisement.GetAdvertisements(context) })
-	//e.POST("/users", func(context echo.Context) error { return c.Advertisement.CreateUser(context) })
+	e.GET("/advertisements", func(context echo.Context) error { return c.Advertisement.GetAdvertisements(context) })
+	e.GET("/advertisements/:id", func(context echo.Context) error { return c.Advertisement.GetOneAdvertisement(context) })
 
+	e.POST("/create", func(context echo.Context) error { return c.Advertisement.CreateAdvertisement(context) })
+	e.GET("/create", func(context echo.Context) error { return c.Advertisement.CreateAdvertisement(context) })
 	return e
 }
