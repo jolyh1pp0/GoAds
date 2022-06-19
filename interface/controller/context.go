c254af329e0a5992f950ac388178c8ba8004f69e
jolyh1pp0 <54851461+jolyh1pp0@users.noreply.github.com>
2022-06-19 22:28:22 +0300

    upd

--- interface/controller/context.go
+++ interface/controller/context.go
@@ -2,5 +2,7 @@ package controller
 
 type Context interface {
 	JSON(code int, i interface{}) error
+	JSONPretty(code int, i interface{}, indent string) error
+	Param(name string) string
 	Bind(i interface{}) error
 }
