ef7ef8ffc553127f497037fb8b9c47ad76bee836
jolyh1pp0 <54851461+jolyh1pp0@users.noreply.github.com>
2022-06-20 05:19:55 +0300

    added omitempty tag to json

--- domain/model/advertisement.go
+++ domain/model/advertisement.go
@@ -3,15 +3,15 @@ package model
 import "time"
 
 type Advertisement struct {
-	ID          uint       `gorm:"primary_key" json:"id"`
-	Title       string     `json:"title"`
-	Description string     `json:"description"`
-	Price       float32    `json:"price"`
-	Photo_1     string     `json:"photo_1"`
-	Photo_2     string     `json:"photo_2"`
-	Photo_3     string     `json:"photo_3"`
-	CreatedAt   *time.Time `json:"created_at"`
-	UpdatedAt   *time.Time `json:"updated_at"`
+	ID          uint       `gorm:"primary_key" json:"id,omitempty"`
+	Title       string     `json:"title,omitempty"`
+	Description string     `json:"description,omitempty"`
+	Price       float32    `json:"price,omitempty"`
+	Photo_1     string     `json:"photo_1,omitempty"`
+	Photo_2     string     `json:"photo_2,omitempty"`
+	Photo_3     string     `json:"photo_3,omitempty"`
+	CreatedAt   *time.Time `json:"created_at,omitempty"`
+	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
 }
 
 func (Advertisement) TableName() string { return "advertisements" }
