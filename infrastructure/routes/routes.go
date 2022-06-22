9aedbc30a2ce045af8aa5f2823c598a6ec42a817
jolyh1pp0 <54851461+jolyh1pp0@users.noreply.github.com>
2022-06-23 02:04:24 +0300

    updated routes

--- infrastructure/routes/routes.go
+++ infrastructure/routes/routes.go
@@ -13,8 +13,7 @@ func NewRouter(e *echo.Echo, c controller.AppController) *echo.Echo {
 	e.GET("/advertisements", func(context echo.Context) error { return c.Advertisement.GetAdvertisements(context) })
 	e.GET("/advertisements/:id", func(context echo.Context) error { return c.Advertisement.GetOneAdvertisement(context) })
 
-	//e.POST("/create", func(context echo.Context) error { return c.Advertisement.CreateAdvertisement(context) })
-	e.GET("/create", func(context echo.Context) error { return c.Advertisement.CreateAdvertisement(context) })
+	e.POST("/create", func(context echo.Context) error { return c.Advertisement.CreateAdvertisement(context) })
 
 	e.GET("delete/:id", func(context echo.Context) error { return c.Advertisement.DeleteAdvertisement(context) })
 
