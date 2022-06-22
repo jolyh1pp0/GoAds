3c20930778009cab06eedcc1c46ebd4759c8cf65
jolyh1pp0 <54851461+jolyh1pp0@users.noreply.github.com>
2022-06-23 02:04:56 +0300

    updated context

--- interface/controller/context.go
+++ interface/controller/context.go
@@ -3,5 +3,6 @@ package controller
 type Context interface {
 	JSONPretty(code int, i interface{}, indent string) error
 	Param(name string) string
-	Bind(i interface{}) error
+	FormValue(name string) string
+	QueryParam(name string) string
 }
