3ad41cf6559ccf7738da6f040d0f2e7db010ac30
jolyh1pp0 <54851461+jolyh1pp0@users.noreply.github.com>
2022-07-11 19:47:14 +0300

    added new routes for users

--- infrastructure/routes/routes.go
+++ infrastructure/routes/routes.go
@@ -12,12 +12,15 @@ func NewRouter(e *echo.Echo, c controller.AppController) *echo.Echo {
 
 	e.GET("/advertisements", func(context echo.Context) error { return c.Advertisement.GetAdvertisements(context) })
 	e.GET("/advertisements/:id", func(context echo.Context) error { return c.Advertisement.GetOneAdvertisement(context) })
-
 	e.PUT("/advertisements/:id", func(context echo.Context) error { return c.Advertisement.UpdateAdvertisement(context) })
-
 	e.POST("/advertisements", func(context echo.Context) error { return c.Advertisement.CreateAdvertisement(context) })
-
 	e.DELETE("advertisements/:id", func(context echo.Context) error { return c.Advertisement.DeleteAdvertisement(context) })
+
+	e.GET("/users", func(context echo.Context) error { return c.User.GetUsers(context) })
+	e.GET("/users/:id", func(context echo.Context) error { return c.User.GetOneUser(context) })
+	e.PUT("/users/:id", func(context echo.Context) error { return c.User.UpdateUser(context) })
+	e.POST("/users", func(context echo.Context) error { return c.User.CreateUser(context) })
+	e.DELETE("users/:id", func(context echo.Context) error { return c.User.DeleteUser(context) })
 
 	return e
 }
