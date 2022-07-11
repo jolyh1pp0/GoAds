3c93a5d2ba5e1a76ff0f6813c66fc126ed85c0a7
jolyh1pp0 <54851461+jolyh1pp0@users.noreply.github.com>
2022-07-11 19:45:54 +0300

    added user table migrations

---
+++ db/migrations/000003_create_users_table.up.sql
@@ -0,0 +1,20 @@
+CREATE TYPE verified_types AS ENUM('email', 'phone', 'google');
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
+
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    time_zone VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) UNIQUE,
+    disabled BOOLEAN NOT NULL DEFAULT false,
+
+    verified_type verified_types,
+    google_secret VARCHAR(255),
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+)
