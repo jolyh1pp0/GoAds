1dab89e64cb7727823fa51e17e9265735bd4cf31
jolyh1pp0 <54851461+jolyh1pp0@users.noreply.github.com>
2022-07-24 21:11:50 +0300

    Add logrus

--- main.go
+++ main.go
@@ -7,8 +7,23 @@ import (
 	"GoAds/registry"
 	"fmt"
 	"github.com/labstack/echo"
+	logrus "github.com/sirupsen/logrus"
 	"log"
+	"os"
+	"time"
 )
+
+func init() {
+	logrus.SetOutput(os.Stdout)
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+
+	logLevel, err := logrus.ParseLevel(os.Getenv("LOG_LEVEL"))
+	if err != nil {
+		logLevel = logrus.InfoLevel
+	}
+
+	logrus.SetLevel(logLevel)
+}
 
 func main() {
 	config.ReadConfig()
@@ -20,10 +35,31 @@ func main() {
 	r := registry.NewRegistry(db)
 
 	e := echo.New()
+
+	e.Use(loggingMiddleware)
+
 	e = routes.NewRouter(e, r.NewAppController())
 
 	fmt.Println("Server listen at http://localhost" + ":" + config.C.Server.Address)
 	if err := e.Start(":" + config.C.Server.Address); err != nil {
 		log.Fatalln(err)
+	}
+}
+
+func loggingMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		start := time.Now()
+
+		res := next(c)
+
+		logrus.WithFields(logrus.Fields{
+			"method":     c.Request().Method,
+			"path":       c.Path(),
+			"status":     c.Response().Status,
+			"latency_ns": time.Since(start).Nanoseconds(),
+			"user_ip":    c.Request().RemoteAddr,
+		}).Info("request details")
+
+		return res
 	}
 }
