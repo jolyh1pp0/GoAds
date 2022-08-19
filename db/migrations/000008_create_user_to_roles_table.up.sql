fcf303cffe797a0ffc6ad7d4dfb5fb13e2328577
shvedovi <54851461+jolyh1pp0@users.noreply.github.com>
2022-08-19 19:11:22 +0300

    added roles and user_to_roles tables

---
+++ db/migrations/000008_create_user_to_roles_table.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE user_to_roles (
+    id SERIAL NOT NULL PRIMARY KEY,
+    user_id UUID NOT NULL,
+    role_id INT NOT NULL,
+
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+            REFERENCES users(id),
+
+    CONSTRAINT fk_role
+        FOREIGN KEY(role_id)
+            REFERENCES roles(id)
+);
